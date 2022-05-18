91aa2290afa3b6a3d1f00ed33f9eb1adc269f515
socrobotics <71850829+socrobotics@users.noreply.github.com>
2022-05-18 18:56:44 +0300

    фиксы

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -9,4 +9,5 @@ async def bot_start(message: types.Message):
     await message.answer(f"Привет, <b>{message.from_user.full_name}</>!\n\n"
                          f"Для начала работы, отправь мне пожалуйста твой токен от сервиса kopeechka.store\n\n"
                          f"<b>P.S.</> Токен состоит из 32 симоволов, прошу не ошибаться\n\n"
-                         f"<i>Пример</>: <b>05fa02c8710c2f60d617587ae69bf91a</>")
+                         f"<i>Пример</>: <b>0fad02c8710c2f61d612547ae69bf91a</>",
+                         reply_markup=types.ReplyKeyboardRemove())
